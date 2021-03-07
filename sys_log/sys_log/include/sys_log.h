793823542065cccbdae553434b001b8e1879b9e8
laoyao <laoyaodada@outlook.com>
2021-03-07 17:20:07 +0800

    忽略类型匹配警告

--- sys_log/sys_log/include/sys_log.h
+++ sys_log/sys_log/include/sys_log.h
@@ -2,13 +2,14 @@
 #define _SYS_LOG_H
 
 #include <stdarg.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <syslog.h>
 
-extern void Log_Degug(const char *fmt, ...);
-extern void Log_Warning(const char *fmt, ...);
-extern void Log_Err(const char *fmt, ...);
-extern void Log_Emerg(const char *fmt, ...);
+extern void Log_Degug(const uint8_t *fmt, ...);
+extern void Log_Warning(const uint8_t *fmt, ...);
+extern void Log_Err(const uint8_t *fmt, ...);
+extern void Log_Emerg(const uint8_t *fmt, ...);
 
 #endif
